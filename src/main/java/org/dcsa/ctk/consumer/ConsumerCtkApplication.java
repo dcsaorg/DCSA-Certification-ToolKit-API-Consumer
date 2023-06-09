44db7dcc2c61086ad6833e13595fa6d94f6e322c
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2023-06-09 09:37:14 +0200

    Cleanup

--- src/main/java/org/dcsa/ctk/consumer/ConsumerCtkApplication.java
+++ src/main/java/org/dcsa/ctk/consumer/ConsumerCtkApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ConsumerCtkApplication {
